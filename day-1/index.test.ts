f50ba04d4acaacdc3c75be6c5561a112491a3c86
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2024-12-15 19:33:35 +0700

    finish day-1 part-1

--- day-1/index.test.ts
+++ day-1/index.test.ts
@@ -1,9 +1,31 @@
 import { expect, test } from "vitest";
+import { readFileSync } from "fs";
 
-function sum(a: number, b: number) {
-  return a + b;
+function totalDistance(textFile: string) {
+  const arr1: number[] = [];
+  const arr2: number[] = [];
+
+  textFile.split("\n").forEach((text) => {
+    const splitText = text.split("   ");
+    arr1.push(parseInt(splitText[0], 10));
+    arr2.push(parseInt(splitText[splitText.length - 1], 10));
+  });
+
+  const sortedArr1 = arr1.toSorted((a, b) => a - b);
+  const sortedArr2 = arr2.toSorted((a, b) => a - b);
+
+  return sortedArr1.reduce(
+    (acc, cur, index) => acc + Math.abs(cur - sortedArr2[index]),
+    0
+  );
 }
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("should return 11 from example", () => {
+  const text = readFileSync("./day-1/example.txt", "utf8");
+  expect(totalDistance(text)).toBe(11);
+});
+
+test("Part 1: should return 2430334 from input", () => {
+  const text = readFileSync("./day-1/input.txt", "utf8");
+  expect(totalDistance(text)).toBe(2430334);
 });
